a702b514b9d193946f14d21a85ee7dcf8ceca1bc
MaiconGiehl <maicongiehl@gmail.com>
2022-10-21 22:27:06 -0300

    limpar, apagar ultimo e zerar

--- components/calculadora-keys.tsx
+++ components/calculadora-keys.tsx
@@ -1,29 +1,92 @@
 import React, { useState } from "react";
 import { Text, View, Image, TouchableHighlight } from "react-native";
+import { Float } from "react-native/Libraries/Types/CodegenTypes";
 import { keysStyles } from "../assets/styles/keys.style";
 
 export function CalculadoraKeys() {
-  const [digitos, onChangeDigitos] = useState("");
+  const [digitos, onChangeDigitos] = useState([""]);
   const [expressao, onChangeExpressao] = useState("");
-  const [resultados, onChangeResultados] = useState("");
+  const [result, onChangeResult] = useState(0);
 
   const digito = (valor: number) => {
-    const novoValor = digitos.concat(String(valor));
-    onChangeDigitos(novoValor);
+    if (!expressao) {
+      const novoValor = digitos[0].concat(String(valor));
+      onChangeDigitos([novoValor]);
+      console.log([novoValor]);
+    } else {
+      if (!digitos[1]) {
+        digitos[1] = "";
+      }
+      digitos[1] = digitos[1].concat(String(valor));
+      onChangeDigitos(digitos);
+      console.log(digitos);
+    }
   };
 
-  const resultado = (valores: string, expressao: string) => {
-    let result: string = String(
-      parseFloat(`${valores} ${expressao} ${valores}`)
-    );
-    onChangeResultados(result);
-    console.log(resultados);
+  const expression = (exp: string) => {
+    if (!expressao) {
+      const novaExpression = exp;
+      onChangeExpressao(novaExpression);
+      console.log(novaExpression);
+    }
+
+    console.log(digitos);
+  };
+
+  const limpar = () => {
+    onChangeDigitos([""]);
+    onChangeExpressao("");
+  };
+
+  const apagar = () => {
+    // let lastConj = digitos[digitos.length - 1];
+    if (!expressao) {
+      digitos[0] = digitos[0].substring(0, digitos[0].length - 1);
+    } else {
+      digitos[1] = digitos[1].substring(0, digitos[1].length - 1);
+    }
+
+    onChangeDigitos(digitos);
+  };
+
+  const zerar = () => {
+    if (!expressao) {
+      digitos[0] = "";
+    } else {
+      digitos[1] = "";
+    }
+
+    onChangeDigitos(digitos);
+  };
+
+  const calcular = () => {
+    let resultado = 0;
+    switch (expressao) {
+      case "/":
+        resultado = Number(digitos[0]) / Number(digitos[1]);
+        break;
+      case "x":
+        resultado = Number(digitos[0]) * Number(digitos[1]);
+        break;
+      case "-":
+        resultado = Number(digitos[0]) - Number(digitos[1]);
+        break;
+      case "+":
+        resultado = Number(digitos[0]) + Number(digitos[1]);
+        break;
+    }
+    limpar();
+    onChangeResult(resultado);
+    console.log(result);
   };
 
   return (
     <View>
       <View style={keysStyles.rowKeys}>
-        <TouchableHighlight style={keysStyles.columnKey}>
+        <TouchableHighlight
+          style={keysStyles.columnKey}
+          onPress={() => zerar()}
+        >
           <Text style={keysStyles.keyText}>CE</Text>
         </TouchableHighlight>
 
@@ -31,7 +94,10 @@ export function CalculadoraKeys() {
           <Text style={keysStyles.keyText}>C</Text>
         </TouchableHighlight>
 
-        <TouchableHighlight style={keysStyles.columnKey}>
+        <TouchableHighlight
+          style={keysStyles.columnKey}
+          onPress={() => apagar()}
+        >
           <Image
             source={require("../assets/backspace.png")}
             style={keysStyles.backspace}
@@ -39,7 +105,7 @@ export function CalculadoraKeys() {
         </TouchableHighlight>
         <TouchableHighlight
           style={keysStyles.columnKey}
-          onPress={() => onChangeExpressao("/")}
+          onPress={() => expression("/")}
         >
           <Text style={keysStyles.keyText}>/</Text>
         </TouchableHighlight>
@@ -65,7 +131,7 @@ export function CalculadoraKeys() {
         </TouchableHighlight>
         <TouchableHighlight
           style={keysStyles.columnKey}
-          onPress={() => onChangeExpressao("*")}
+          onPress={() => expression("x")}
         >
           <Text style={keysStyles.keyText}>X</Text>
         </TouchableHighlight>
@@ -91,7 +157,7 @@ export function CalculadoraKeys() {
         </TouchableHighlight>
         <TouchableHighlight
           style={keysStyles.columnKey}
-          onPress={() => onChangeExpressao("-")}
+          onPress={() => expression("-")}
         >
           <Text style={keysStyles.keyText}>-</Text>
         </TouchableHighlight>
@@ -117,7 +183,7 @@ export function CalculadoraKeys() {
         </TouchableHighlight>
         <TouchableHighlight
           style={keysStyles.columnKey}
-          onPress={() => onChangeExpressao("+")}
+          onPress={() => expression("+")}
         >
           <Text style={keysStyles.keyText}>+</Text>
         </TouchableHighlight>
@@ -137,7 +203,7 @@ export function CalculadoraKeys() {
         </TouchableHighlight>
         <TouchableHighlight
           style={keysStyles.columnKey}
-          onPress={() => resultado(digitos, expressao)}
+          onPress={() => calcular()}
         >
           <Text style={keysStyles.keyText}>=</Text>
         </TouchableHighlight>
